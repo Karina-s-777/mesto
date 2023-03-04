91ece5ef4ba86128779babfea03b4cb274cfafa4
Karina Savilova <savilovakarina@yandex.ru>
2023-03-04 15:23:57 +0200

    feat: Внести функцию смены имени и описания

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log('Hellow, world!');
+// вводим переменную popupElement, которой присваиваем селектор .popup// 
+const popupElement = document.querySelector(".popup")
+const popupButtonCloseElement = popupElement.querySelector(".popup__button-close")
+const popupButtonOpenElement = document.querySelector(".profile__edit-button")
+
+const nameInput = popupElement.querySelector(".popup__item-name")
+const jobInput = popupElement.querySelector(".popup__item-about")
+const nameProfile = document.querySelector(".profile__user-name")
+const jobProfile = document.querySelector(".profile__user-info")
+const formElement = popupElement.querySelector(".popup__form") 
+
+const togglePopupVisibility = function () {
+    popupElement.classList.toggle("popup_opened")
+    }
+    
+const popupOpen = function () {
+    popupElement.classList.add("popup_opened")
+    nameInput.value = nameProfile.textContent;
+    jobInput.value = jobProfile.textContent;  
+}
+
+const popupClose = function () {
+    popupElement.classList.remove("popup_opened")
+}
+
+popupButtonOpenElement.addEventListener('click', togglePopupVisibility)
+
+const handleFormSubmit = function (evt) { 
+    evt.preventDefault();
+    jobProfile.textContent = jobInput.value; 
+    nameProfile.textContent = nameInput.value;
+    popupClose ()
+}
+
+formElement.addEventListener('submit', handleFormSubmit); 
+
+
